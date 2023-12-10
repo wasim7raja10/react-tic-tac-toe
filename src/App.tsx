81192c1d3534ae9efcfd483cf4c795715c61a1cc
Wasim Raja <wasim7raja10@gmail.com>
2023-12-10 20:07:22 +0530

    Refactor Board component and add game reset functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import Board from "./Board";
 import Moves from "./Moves";
 
 function App() {
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [moveNumber, setMoveNumber] = useState(0);
+	const currentBoardState = history[history.length - 1];
+
+	function handlePlay(nextBoardState: string[]) {
+		setHistory([...history, nextBoardState]);
+    setMoveNumber(moveNumber + 1);
+	}
+
+  function handleResetGame() {
+    setHistory([Array(9).fill(null)]);
+    setMoveNumber(0);
+  }
+  
 	return (
 		<>
 			<div className="flex gap">
 				{/* Game */}
 				<div>
-					<Board />
+					<Board
+						currentBoardState={currentBoardState}
+						onPlay={handlePlay}
+						currentMoveNumber={moveNumber}
+					/>
+          <button onClick={handleResetGame} className="reset">RESET GAME</button>
 				</div>
 
 				{/* Move */}
--- src/Board.tsx
+++ src/Board.tsx
@@ -1,5 +1,44 @@
-function Board() {
-	return <div>Board</div>;
+import { calculateWinner } from "./lib/helper";
+
+function Board({
+	currentBoardState,
+	onPlay,
+	currentMoveNumber,
+}: {
+	currentBoardState: string[];
+	onPlay: (nextBoardState: string[]) => void;
+	currentMoveNumber: number;
+}) {
+	function handleClick(index: number) {
+		if (currentBoardState[index] || calculateWinner(currentBoardState)) {
+			return;
+		}
+		const nextBoardState = [...currentBoardState];
+		nextBoardState[index] = currentMoveNumber % 2 === 0 ? "X" : "O";
+		onPlay(nextBoardState);
+	}
+
+	const winner = calculateWinner(currentBoardState);
+	const status = winner
+		? `Winner: ${winner}`
+		: `Next player: ${currentMoveNumber % 2 === 0 ? "X" : "O"}`;
+
+	return (
+		<>
+			<h6 className="status">{status}</h6>
+			<div className="board">
+				{currentBoardState.map((square: string, index: number) => (
+					<button
+						className="square"
+						onClick={() => handleClick(index)}
+						key={index}
+					>
+						{square}
+					</button>
+				))}
+			</div>
+		</>
+	);
 }
 
 export default Board;
