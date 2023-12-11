8a17d52bdf34fe84a021c81cd78b05e47ec396eb
Wasim Raja <wasim7raja10@gmail.com>
2023-12-11 21:29:59 +0530

    Add sorting functionality to history list

--- src/Moves.tsx
+++ src/Moves.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BoardState } from "./lib/types";
 
 function History({
@@ -7,25 +8,49 @@ function History({
 	onJumpTo: (move: number) => void;
 	history: BoardState[];
 }) {
+	const [order, setOrder] = useState<"ascending" | "descending">("ascending");
+
 	if (history[history.length - 1].moveLocation === null) {
 		return <h6>GAME not started</h6>;
 	}
+
+	const toggleOrder = () => {
+		setOrder(order === "ascending" ? "descending" : "ascending");
+	};
+
+	const orderedHistory =
+		order === "ascending" ? history : [...history].reverse();
+
+	const islatestMove = (moveIndex: number) => {
+		return moveIndex === history.length - 1;
+	};
+
+	const moveIndex = (index: number) => {
+		if (order === "ascending") {
+			return index;
+		}
+		return history.length - 1 - index;
+	};
+
 	return (
 		<div>
-			<div>
-				<h6 style={{ fontSize: "1rem" }}>History</h6>
+			<div className="flex">
+				<h6 style={{ fontSize: "1rem", marginRight: "1rem" }}>History</h6>
+				<button onClick={toggleOrder}>
+					Sort in {order === "ascending" ? "descending" : "ascending"}
+				</button>
 			</div>
 			<ul className="list">
-				{history.map(({ moveLocation }, move) => {
+				{orderedHistory.map(({ moveLocation }, index) => {
 					return (
-						<li key={move}>
-							{move === history.length - 1 ? (
-								<b>{`Move #${move} - (${moveLocation})`}</b>
+						<li key={moveIndex(index)}>
+							{islatestMove(moveIndex(index)) ? (
+								<b>{`Move #${moveIndex(index)} - (${moveLocation})`}</b>
 							) : (
-								<button onClick={() => onJumpTo(move)}>
-									{move === 0
+								<button onClick={() => onJumpTo(moveIndex(index))}>
+									{moveIndex(index) === 0
 										? "Go to game start"
-										: `Go to move #${move} - (${moveLocation})`}
+										: `Go to move #${moveIndex(index)} - (${moveLocation})`}
 								</button>
 							)}
 						</li>
